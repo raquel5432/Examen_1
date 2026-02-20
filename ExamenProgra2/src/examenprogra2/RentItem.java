343064eaf20635473b7e4f437f87e7d8d5d458ac
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-02-20 09:16:58 -0600

    m

--- ExamenProgra2/src/examenprogra2/RentItem.java
+++ ExamenProgra2/src/examenprogra2/RentItem.java
@@ -4,10 +4,53 @@
  */
 package examenprogra2;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author ALISSONRAQUELMARTINE
  */
-public class RentItem {
+public abstract class RentItem {
+ 
+    protected String Code;
+    protected String Name;
+    protected double precioBase;
+    protected int copies;
+    protected ImageIcon image;
     
+    
+    public RentItem(String Code, String Name, double precioBase, int copies, ImageIcon image){
+        
+        this.Code = Code;
+        this.Name = Name;
+        this.copies = 0;
+        this.precioBase = precioBase;
+        this.image = image;
+  
+    }
+    
+    public String getCode(){
+        return Code;
+    }
+    
+    public String getName(){
+        return Name;
+    }
+    
+    public double getPreciobase(){
+        return precioBase;
+    }
+    
+    public ImageIcon getImaged(){
+        return image;
+    }
+    
+    public String ToString(){
+        return "Codigo: " + Code +
+               "\nNombre: " + Name +
+               "\nPrecio Base: " + precioBase +
+               "\nCopias: " + copies;
+    }
+    
+    public abstract double pagoRenta(int dias);
 }
